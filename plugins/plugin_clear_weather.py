f93c94bf8261e826edfd39b161ed24ab40d697d7
justcallmekoko <at.the.red.team@gmail.com>
2020-07-30 22:36:41 -0400

    Add arrow mods

---
+++ plugins/plugin_clear_weather.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from mcrcon import MCRcon
+from discord.ext.tasks import loop
+
+PASSW = os.getenv('RCON_PASSWORD')
+
+class ClearWeather():
+	name = '!clearweather'
+
+	desc = 'Enable clear weather only'
+
+	synt = '!clearweather <on|off|status>'
+
+	looping = False
+
+	admin = True
+
+	@loop(seconds = 1)
+	async def loop_func(self):
+		if self.looping:
+                        with MCRcon("127.0.0.1", PASSW) as mcr:
+                                resp = mcr.command('/weather clear')
+                                #print (resp)
+                                mcr.disconnect()
+
+	async def run(self, message):
+		if message.content.split(' ')[1].lower() == 'on' and not self.looping:
+			print ('Running clear weather on...')
+			self.looping = True
+			self.loop_func.start()
+		elif message.content.split(' ')[1].lower() == 'off' and self.looping:
+			print ('Running clear weather off...')
+			self.looping = False
+			self.loop_func.stop()
+		elif message.content.split(' ')[1].lower() == 'status':
+			if self.looping:
+				await message.channel.send(message.author.mention + ' !clearweather is on')
+			else:
+				await message.channel.send(message.author.mention + ' !clearweather is off')
+
+		else:
+			await message.channel.send(message.author.mention + ' ' + str(message.content) + ' is not a recognized command')
---
+++ plugins/plugin_exploding_arrows.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from mcrcon import MCRcon
+from discord.ext.tasks import loop
+
+PASSW = os.getenv('RCON_PASSWORD')
+
+class ExplodingArrows():
+	name = '!explodingarrows'
+
+	desc = 'Arrows explode when they hit the ground'
+
+	synt = '!explodingarrows <on|off|status>'
+
+	looping = False
+
+	admin = True
+
+	@loop(seconds = 0.1)
+	async def loop_func(self):
+		if self.looping:
+			with MCRcon("127.0.0.1", PASSW) as mcr:
+				resp = mcr.command('/execute at @e[type=arrow,nbt={inGround:1b}] run summon tnt')
+				resp = mcr.command('/kill @e[type=arrow,nbt={inGround:1b}]')
+				#print (resp)
+				mcr.disconnect()
+
+	async def run(self, message):
+		if message.content.split(' ')[1].lower() == 'on' and not self.looping:
+			print ('Running Exploding arrows on...')
+			self.looping = True
+			self.loop_func.start()
+			await message.channel.send(message.author.mention + ' !explodingarrows enabled')
+		elif message.content.split(' ')[1].lower() == 'off' and self.looping:
+			print ('Running Exploding arrows off...')
+			self.looping = False
+			self.loop_func.stop()
+			await message.channel.send(message.author.mention + ' !explodingarrows disabled')
+		elif message.content.split(' ')[1].lower() == 'status':
+			if self.looping:
+				await message.channel.send(message.author.mention + ' !explodingarrows is on')
+			else:
+				await message.channel.send(message.author.mention + ' !explodingarrows is off')
+
+		else:
+			await message.channel.send(message.author.mention + ' ' + str(message.content) + ' is not a recognized command')
